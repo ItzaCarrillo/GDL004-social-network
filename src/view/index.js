bc1cf3b71ddf7482c26bd762d2f465f688115f0a
ItzaCarrillo <56931707+ItzaCarrillo@users.noreply.github.com>
2020-01-23 12:27:27 -0600

    Revert "header and navbar en commonElements.js"

--- src/view/index.js
+++ src/view/index.js
@@ -1,13 +1,11 @@
 
 import Login from './login.js';
-import FormCount from './formCount.js';
 import Home from './home.js';
 import Profile from './profile.js';
 import EditProfile from './Edit-Profile.js';
 
 const components = {
     login: Login,
-    formCount: FormCount,
     home: Home,
     profile: Profile,
     editProfile: EditProfile
